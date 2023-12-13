710946074fe745d9f279b0bf35c76b0d180fbf56
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-12-13 10:42:23 -0500

    Agrega hotjar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -53,5 +53,17 @@ export default defineConfig({
       {},
       "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-V13H3VTPBL');",
     ],
+    [
+      'script',
+      {},
+      `(function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:3791609,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+    ],
   ],
 })
