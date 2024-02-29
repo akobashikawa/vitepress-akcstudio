813116b726bb780c43049fa1628992f2bbc3dead
Antonio Kobashikawa <akobashikawa@gmail.com>
2024-02-29 00:30:28 -0500

    Agrega tawk widget

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -126,6 +126,11 @@ export default withMermaid({
       {},
       "(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:3791609,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');",
     ],
+    [
+      'script',
+      {},
+      "var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); (function(){ var s1=document.createElement('script'),s0=document.getElementsByTagName('script')[0]; s1.async=true; s1.src='https://embed.tawk.to/65e0139d8d261e1b5f66ca34/1hnpkp8ge'; s1.charset='UTF-8'; s1.setAttribute('crossorigin','*'); s0.parentNode.insertBefore(s1,s0); })();",
+    ],
   ],
   // optionally, you can pass MermaidConfig
   mermaid: {
